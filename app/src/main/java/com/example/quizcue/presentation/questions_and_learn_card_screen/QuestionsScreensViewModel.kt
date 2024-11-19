d3009b776f5e92bfd3bbe6bf5c470c3ac7f3e24f
Xenia_Popova <p961083@gmail.com>
2024-11-19 22:08:39 +0300

    Added QuestionsScreens VM

---
+++ app/src/main/java/com/example/quizcue/presentation/questions_and_learn_card_screen/QuestionsScreensViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.quizcue.presentation.questions_and_learn_card_screen
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.quizcue.domain.model.Question
+import com.example.quizcue.domain.repository.QuestionRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class QuestionsScreensViewModel @Inject constructor(
+    private val questionRepository: QuestionRepository,
+    savedStateHandler: SavedStateHandle
+): ViewModel() {
+
+    private val _questions = MutableStateFlow<List<Question>>(emptyList())
+    val questions: StateFlow<List<Question>> = _questions
+
+    private val courseQuestion: String = savedStateHandler["courseId"] ?: ""
+
+    init {
+        getQuestions()
+    }
+
+    private fun getQuestions() {
+        viewModelScope.launch {
+            questionRepository.getQuestions()
+                .collect{ _questions.value = it }
+            _questions.value.filter { it.course == courseQuestion }
+        }
+    }
+}
